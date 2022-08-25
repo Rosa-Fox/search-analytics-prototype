7e328f0a4e0fc6939bc3cfe24362672fcc2bdc0d
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2022-08-25 19:21:20 +0100

    Build up a collection of all data
    Will need a collection of all data so that
    ranking can be calculated on all of the data,
    not just per results on each individual page
    
    Ranks will be calculated in a later commit.

--- fetch.rb
+++ fetch.rb
@@ -2,14 +2,20 @@ require_relative 'client'
 require 'pry'
 
 class Fetch
-  attr_reader :client
+  attr_reader :client, :all_data
   def initialize
     @client = Client.new("0")
+    @all_data = []
   end
 
   def get_ga_data(page_token)
     client = Client.new(page_token)
     client.service.authorization = client.credentials
+
+    # Build up a collection of all data so that
+    # ranking will be calculated on all of the data
+    # not just per paginated page
+    all_data << client.responses
 
     page_token = client.response.reports.first.to_h[:next_page_token]
 
