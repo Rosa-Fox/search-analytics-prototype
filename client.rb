90c6b17cc506fed9f585d96f0c268c338b7f1a09
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2022-08-25 18:43:13 +0100

    Create GA Client
    Fetches Google Analytics data for all GOV.UK pages.
    Returns a response.

---
+++ client.rb
@@ -0,0 +1,75 @@
+require 'google/apis/analyticsreporting_v4'
+require 'pry'
+
+class Client
+  attr_reader :service, :credentials
+  def initialize
+        @service = Google::Apis::AnalyticsreportingV4::AnalyticsReportingService.new
+        @credentials = Google::Auth::ServiceAccountCredentials.make_creds(
+          json_key_io: File.open('keyfile.json'),
+          scope: "https://www.googleapis.com/auth/analytics.readonly"
+        )
+    end
+    
+   # Set the date range - this is always required for report requests
+  def date_range
+    Google::Apis::AnalyticsreportingV4::DateRange.new(
+      start_date: "2022-08-21",
+      end_date: "2022-08-21"
+    )
+  end
+
+  # Set the metric
+  def metric
+    Google::Apis::AnalyticsreportingV4::Metric.new(
+      expression: "ga:uniquePageViews"
+    )
+  end
+
+  #Set the pagePath dimension
+  def dimension_path
+    Google::Apis::AnalyticsreportingV4::Dimension.new(
+      name: "ga:pagePath"
+    )
+  end
+
+  #Set the pageTitle dimension
+  def dimension_title
+    Google::Apis::AnalyticsreportingV4::Dimension.new(
+      name: "ga:pageTitle"
+    )
+  end
+
+  # Build GA request
+  def report_request
+    Google::Apis::AnalyticsreportingV4::ReportRequest.new(
+      view_id: 'ga:56562468',
+      sampling_level: 'LARGE',
+      date_ranges: [date_range],
+      metrics: [metric],
+      dimensions: [dimension_path, dimension_title],
+      page_size: "1000"
+    )
+  end
+
+  # Create a new report request
+  def request
+    service.authorization = credentials
+    Google::Apis::AnalyticsreportingV4::GetReportsRequest.new(
+      { report_requests: [report_request] }
+    )
+  end
+
+  # Response from GA
+  def response
+    service.batch_get_reports(request)
+  end
+
+  # Get data from the response
+  def responses
+    response.reports.first.to_h
+  end
+end
+
+client = Client.new
+client.responses
